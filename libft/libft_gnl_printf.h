7e1e5b66eb5487cdb383ab293830ca165507bfe1
rmiranda <rmiranda@student.42sp.org.br>
2022-10-27 09:57:21 +0200

    re-adding libft part 2/2

---
+++ libft/libft_gnl_printf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft_gnl_printf.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmiranda <rmiranda@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/27 08:17:07 by rmiranda          #+#    #+#             */
+/*   Updated: 2022/08/27 08:17:15 by rmiranda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_GNL_PRINTF_H
+# define LIBFT_GNL_PRINTF_H
+
+# include "./libft/libft.h"
+# include "./printf/ft_printf.h"
+# include "./gnl/get_next_line.h"
+
+#endif
